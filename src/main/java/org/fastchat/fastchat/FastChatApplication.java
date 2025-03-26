2d67cbe72a54fab53f96c73a503933e35a0f6308
younggyun12 <younggyun12@hotmail.com>
2025-03-26 16:48:22 +0900

    Initial commit: 프로젝트 생성 및 종속성 추가

---
+++ src/main/java/org/fastchat/fastchat/FastChatApplication.java
@@ -0,0 +1,13 @@
+package org.fastchat.fastchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fastchat/fastchat/FastChatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fastchat.fastchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
